47ba9fc25e59504f05e9b6f27bce4cc6f9f00496
Daniel Thatcher <dcthatch@gmail.com>
2017-01-21 12:38:41 +0000

    Removed broken colour setting and added random wallpaper to lightdm greeter

--- .i3/rand_wallpaper.sh
+++ .i3/rand_wallpaper.sh
@@ -2,19 +2,18 @@
 
 #Uses nitrogen to set a random wallpaper on two monitors from two different source directories
 
-source1='/home/daniel/Documents/Wallpapers/16-9/'
-#source2='/home/daniel/Documents/Wallpapers/21-9/'
-#source1='/home/daniel/Documents/Wallpapers/21-9/'
+source='/home/daniel/Documents/Wallpapers/16-9/'
 
-image1=$(ls $source1 | sort -R | tail -1)
-#image2=$(ls $source2 | sort -R | tail -1)
+image1=$(ls $source | sort -R | tail -1)
+image2=$(ls $source | sort -R | tail -1)
 
-#Currently requires nitrogen installed from git for --head option
-nitrogen --set-scaled --head=0 "$source1$image1"
-#nitrogen --set-scaled --head=1 "$source2$image2"
+nitrogen --set-scaled --head=0 "$source$image1"
+
+# For the gtk lightdm greeter
+cp $source$image2 /usr/share/pixmaps/greeter_wallpaper
 
 #Load terminal colours from images
-if ! [ -f "/home/daniel/.wallpapers/$image2" ]; then
-	zsh /home/daniel/.i3/wp/wp add "$source2$image2"
-fi
-zsh /home/daniel/.i3/wp/wp change "$image2"
+#if ! [ -f "/home/daniel/.wallpapers/$image2" ]; then
+	#zsh /home/daniel/.i3/wp/wp add "$source2$image2"
+#fi
+#zsh /home/daniel/.i3/wp/wp change "$image2"
